b2283e03867c3ab6c112e5df4d4a489e3794ec60
Amine Lahlou <aminelahlou5@gmail.com>
2016-04-17 15:23:24 +0200

    hello sarina bug

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ app.get('/', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-  hello sarina
 });
 
 app.post('/contact',function(request, response){
